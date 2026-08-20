5d23f82ac3d7ab5bdd3ec9c75aceef71cb9b6c5a
Robinhoets <robertets88@gmail.com>
2026-08-20 13:53:34 -0400

    SPI data structures and addresses setup

--- drivers/Inc/stm32f407xx.h
+++ drivers/Inc/stm32f407xx.h
@@ -176,6 +176,22 @@ typedef struct
 }SYSCFG_RegDef_t;
 
 /*
+ * 	SPI register definition structure
+ */
+typedef struct
+{
+	__vo uint32_t CR1;					/*!< SPI control register 1,										Address offset: 0x00 >*/
+	__vo uint32_t CR2;					/*!< SPI control register 2,										Address offset: 0x04 >*/
+	__vo uint32_t SR;					/*!< SPI status register,											Address offset: 0x08 >*/
+	__vo uint32_t DR;					/*!< SPI data register,												Address offset: 0x0C >*/
+	__vo uint32_t CRCPR;				/*!< SPI CRC polynomial register,									Address offset: 0x10 >*/
+	__vo uint32_t RXCRCR;				/*!< SPI RX CRC register,											Address offset: 0x14 >*/
+	__vo uint32_t TXCRCR;				/*!< SPI TX CRC register,											Address offset: 0x18 >*/
+	__vo uint32_t I2SCFGR;				/*!< SPI_I2S configuration register,								Address offset: 0x1C >*/
+	__vo uint32_t I2SPR;				/*!< SPI_I2S prescaler register,									Address offset: 0x20 >*/
+}SPI_RegDef_t;
+
+/*
  * 	Peripheral definitions.
  * 	Base addresses typecasted to xxx_RegDef_t
  */
@@ -194,6 +210,10 @@ typedef struct
 #define EXTI			((EXTI_RegDef_t*) EXTI_BASEADDR)
 
 #define SYSCFG			((SYSCFG_RegDef_t*) SYSCFG_BASEADDR)
+
+#define SPI1			((SPI_RegDef_t*)SPI1_BASE)
+#define SPI2			((SPI_RegDef_t*)SPI2_BASE)
+#define SPI3			((SPI_RegDef_t*)SPI3_BASE)
 
 /*
  * 	Clock Enable Macros for GPIOx peripherals
