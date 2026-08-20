c146efbf6a71d0c9eb17683c991278e42e457b3c
Robinhoets <robertets88@gmail.com>
2026-08-20 14:18:55 -0400

    created function definitions in the header

--- drivers/Inc/stm32f407xx_spi_driver.h
+++ drivers/Inc/stm32f407xx_spi_driver.h
@@ -24,4 +24,38 @@ typedef struct
 	SPI_Config_t	SPIConfig;
 }SPI_Handle_t;
 
+/*****************************************************************************************
+ * 							APIs supported by this driver
+ * 			For more information about the APIs check the function definitions
+ */
+/*
+ * 	Peripheral Clock setup
+ */
+void SPI_PeriClockControl(SPI_RegDef_t *pSPIx, uint8_t EnorDi);
+
+/*
+ * 	Initialize and De-initialize
+ */
+void SPI_Init(SPI_Handle_t *pSPIHandle);
+void SPI_DeInit(SPI_RegDef_t *pSPIx);
+
+/*
+ * 	Data Send and Receive
+ * 	(aside: blocking is non-interrupt based & non-blocking is interrupt based)
+ */
+void SPI_SendData(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer, uint32_t Len);
+void SPI_ReceiveData(SPI_RegDef_t *pSPIx, uint8_t *pRxBuffer, uint32_t Len);
+
+/*
+ * 	IRQ Configuration and ISR Handling
+ */
+void SPI_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnorDi);
+void SPI_IRQPriorityConfig(uint8_t IRQNumber, uint8_t IRQPriority);
+void SPI_IRQHandling(SPI_Handle_t *pHandle);
+
+/*
+ * 	Other Peripheral Control APIs
+ */
+
+
 #endif
