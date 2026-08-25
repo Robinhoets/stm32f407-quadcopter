2a7df6206baecedec68c519d5296b3a82d123de8
Robinhoets <robertets88@gmail.com>
2026-08-25 09:38:03 -0400

    implemented SPI_GPIOInits() for testing

--- Src/006spi_tx_tesing.c
+++ Src/006spi_tx_tesing.c
@@ -5,8 +5,49 @@
  *      Author: Rober
  */
 
+#include "stm32f407xx.h"
+
+/*
+ * SPI2:
+ * 	PB14 -> MOSI
+ * 	PB15 -> MISO
+ * 	PB13 -> SCLK
+ * 	PB12 -> NSS
+ *	ALT function mode : 5
+ */
+
+void SPI_GPIOInits(void)
+{
+	GPIO_Handle_t SPIPins;
+
+	SPIPins.pGPIOx = GPIOB;
+	SPIPins.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_ALTFN;
+	SPIPins.GPIO_PinConfig.GPIO_PinAltFunMode = 5;
+	SPIPins.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	SPIPins.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+	SPIPins.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+
+	// SCLK
+	SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_13;
+	GPIO_Init(&SPIPins);
+
+	// MOSI
+	SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_15;
+	GPIO_Init(&SPIPins);
+
+	// MISO
+	SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_14;
+	GPIO_Init(&SPIPins);
+
+	// NSS
+	SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_13;
+	GPIO_Init(&SPIPins);
+}
+
 int main()
 {
+
+	SPI_GPIOInits();
 	return 0;
 }
 
